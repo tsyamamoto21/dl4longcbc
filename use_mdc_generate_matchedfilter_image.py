b04bbf518730ca54f4e34dc0ef1f5faa68b0334e
tsyamamoto <tak.guruguru.4@gmail.com>
2025-06-26 15:21:03 +0900

    Make MF parallel

--- use_mdc_generate_matchedfilter_image.py
+++ use_mdc_generate_matchedfilter_image.py
@@ -10,6 +10,7 @@ from pycbc.conversions import mass1_from_mchirp_eta, mass2_from_mchirp_eta
 from pycbc.waveform import get_fd_waveform
 from pycbc.filter import matched_filter
 from dl4longcbc.utils import if_not_exist_makedir
+import concurrent.futures
 
 
 def make_snrmap_coarse(snrmap, kfilter):
@@ -21,66 +22,50 @@ def make_snrmap_coarse(snrmap, kfilter):
     return snrmap_coarse
 
 
-def main(args):
+class SignalProcessingParameters:
+    def __init__(self, duration, tsegment, fs, low_frequency_cutoff, tfft, tukey_alpha, width_input, height_input):
+        # Signal properties
+        self.duration = duration
+        self.tsegment = tsegment
+        self.fs = fs
+        self.low_frequency_cutoff = low_frequency_cutoff
+        self.high_frequency_cutoff = self.fs / 2
 
-    outdir = args.outdir
-    if_not_exist_makedir(outdir)
-    file_foreground = args.foreground
-    file_injection = args.injection
+        # FFT for PSD estimation
+        self.tfft = tfft
+        self.toverlap = self.tfft / 2
+        self.fftlength = int(self.tfft / 2)
+        self.overlaplength = int(self.fftlength / 2)
 
-    # Parameters for template matching
-    duration = 16
-    tsegment = 1
-    fs = 2048
-    fhigh = fs / 2
-    low_frequency_cutoff = 20.0
-    fftlength = int(4 * fs)
-    overlap_length = int(fftlength / 2)
-    window = tukey(int(duration * fs), alpha=1.0 / 16.0)
+        # MF params
+        self.mfdatalength = int(self.duration * self.fs)
+        self.tukey_alpha = tukey_alpha
 
-    approximant_tmp = 'IMRPhenomXPHM'
-    mcmin_tmp = 5.0
-    mcmax_tmp = 50.0
-    ngrid_mc = 256
-    mclist = np.logspace(np.log10(mcmin_tmp), np.log10(mcmax_tmp), ngrid_mc, endpoint=True)
-    eta = 0.25
-    a1 = 0.0
-    a2 = 0.0
+        # Image properties
+        self.width_input = width_input
+        self.height_input = height_input
+        self.kfilter = int(self.fs * self.tsegment / self.width_input)
+        self.kcrop_left = int(self.fs * (self.duration / 2 - 3 * self.tsegment / 4))
+        self.kcrop_right = int(self.fs * (self.duration / 2 + 3 * self.tsegment / 4))
+        self.width_before_smearing = self.kcrop_right - self.kcrop_left
 
-    # Image parameters
-    # height = ngrid_mc
-    width_input = 256
-    kfilter = int(fs * tsegment / width_input)
-    kcrop_left = int(fs * (duration / 2 - 3 * tsegment / 4))
-    kcrop_right = int(fs * (duration / 2 + 3 * tsegment / 4))
-    width_before_smearing = kcrop_right - kcrop_left
 
-    # Make a template bank
-    template_bank = []
-    for i in range(ngrid_mc):
-        mass1 = mass1_from_mchirp_eta(mclist[i], eta)
-        mass2 = mass2_from_mchirp_eta(mclist[i], eta)
-        params_tmp = {
-            'approximant': approximant_tmp,
-            'mass1': mass1,
-            'mass2': mass2,
-            'spin1z': a1,
-            'spin2z': a2,
-            'f_lower': low_frequency_cutoff,
-            'delta_f': 1.0 / duration,
-            'f_final': fhigh
-        }
+class SegmentTimestamps:
+    def __init__(self):
+        self.start_time_str: str = None
+        self.start_time: int = None
+        self.end_time: int = None
+        self.tclist_for_short_segment: list = None
 
-        hp_fd, _ = get_fd_waveform(**params_tmp)
-        template_bank.append(hp_fd)
 
+def get_timestamps(file_foreground: str, file_injection: str, sp: SignalProcessingParameters):
     # Get start time and end time
     with h5py.File(file_foreground, 'r') as fo:
         x = fo['H1'].keys()
         start_time_strlist = [xsample for xsample in x]
         start_time_list = [int(s) for s in start_time_strlist]
         nsegment = len(start_time_list)
-        end_time_list = [int(len(fo['H1'][start_time_strlist[i]]) / fs) + start_time_list[i] for i in range(nsegment)]
+        end_time_list = [int(len(fo['H1'][start_time_strlist[i]]) / sp.fs) + start_time_list[i] for i in range(nsegment)]
 
     # Get the injection time stamps
     with h5py.File(file_injection, 'r') as fo:
@@ -92,53 +77,235 @@ def main(args):
             if (start_time_list[n] <= tclist_from_hdf5[idx]) * (tclist_from_hdf5[idx] <= end_time_list[n]):
                 tclist[n].append(tclist_from_hdf5[idx])
 
-    # Run the main code
-    snrlist = torch.zeros((2, ngrid_mc, width_before_smearing), requires_grad=False)
-    dataidx = 0
+    # Store timestamps in instances
+    segment_timestamp_list = []
     for n in range(nsegment):
-        # Set time stampes
-        start_time_str = start_time_strlist[n]
-        start_time = start_time_list[n]
-        end_time = end_time_list[n]
-        tclist_for_short_segment = tclist[n]
-        if args.offevent:
-            tclist_for_short_segment = np.array(tclist_for_short_segment)
-            tclist_for_short_segment = (tclist_for_short_segment[:-1] + tclist_for_short_segment[1:]) / 2
+        st = SegmentTimestamps()
+        st.start_time_str = start_time_strlist[n]
+        st.start_time = start_time_list[n]
+        st.end_time = end_time_list[n]
+        st.tclist_for_short_segment = tclist[n]
+        segment_timestamp_list.append(st)
 
-        # Load a hdf file.
-        xh1 = load_timeseries(file_foreground, group=f'H1/{start_time_str}')
-        xl1 = load_timeseries(file_foreground, group=f'L1/{start_time_str}')
+    return segment_timestamp_list
 
-        # Estimate PSD
-        psd_h = pycbc.psd.welch(xh1, seg_len=fftlength, seg_stride=overlap_length, avg_method='median-mean')
-        psd_h_interp = pycbc.psd.interpolate(psd_h, delta_f=1.0 / duration)
-        psd_l = pycbc.psd.welch(xl1, seg_len=fftlength, seg_stride=overlap_length, avg_method='median-mean')
-        psd_l_interp = pycbc.psd.interpolate(psd_l, delta_f=1.0 / duration)
 
-        for tc in tclist_for_short_segment:
-            tini = tc - duration / 2
-            tfin = tc + duration / 2
-            if (start_time < tini) and (tfin < end_time):
-                print(tc)
-                # Slice the data and window
-                strain_h = xh1.time_slice(tini, tfin) * window
-                strain_l = xl1.time_slice(tini, tfin) * window
+def matchedfilter_core(file_foreground: str, template_bank: list, outdir: str, sp: SignalProcessingParameters, timestamps: SegmentTimestamps):
 
-                # Calculate SNR
-                # rholist = []
-                for i in range(ngrid_mc):
-                    rho_h = matched_filter(template_bank[i], strain_h, psd=psd_h_interp, low_frequency_cutoff=low_frequency_cutoff)
-                    rho_l = matched_filter(template_bank[i], strain_l, psd=psd_l_interp, low_frequency_cutoff=low_frequency_cutoff)
-                    # snrlist[i] = torch.from_numpy(((abs(rho_h)**2 + abs(rho_l)**2) ** 0.5).numpy().astype(np.float32))
-                    snrlist[0, i] = torch.from_numpy(abs(rho_h).numpy())[kcrop_left: kcrop_right]
-                    snrlist[1, i] = torch.from_numpy(abs(rho_l).numpy())[kcrop_left: kcrop_right]
+    # SNR array
+    snrlist = torch.zeros((2, sp.height_input, sp.width_before_smearing), requires_grad=False)
 
-                dataavg = make_snrmap_coarse(snrlist, kfilter).to(torch.float32)
-                torch.save(dataavg, f'{outdir}/inputs_{int(duration):d}_{dataidx:d}.pth')
-                dataidx += 1
-                if dataidx==100:
-                    import sys
-                    sys.exit(1)
+    # Tukey window
+    window = tukey(sp.mfdatalength, sp.tukey_alpha)
+
+    if args.offevent:
+        tclist_for_short_segment = np.array(timestamps.tclist_for_short_segment)
+        tclist_for_short_segment = (tclist_for_short_segment[:-1] + tclist_for_short_segment[1:]) / 2
+
+    # Load a hdf file.
+    xh1 = load_timeseries(file_foreground, group=f'H1/{timestamps.start_time_str}')
+    xl1 = load_timeseries(file_foreground, group=f'L1/{timestamps.start_time_str}')
+
+    # Estimate PSD
+    psd_h = pycbc.psd.welch(xh1, seg_len=sp.fftlength, seg_stride=sp.overlaplength, avg_method='median-mean')
+    psd_h_interp = pycbc.psd.interpolate(psd_h, delta_f=1.0 / sp.duration)
+    psd_l = pycbc.psd.welch(xl1, seg_len=sp.fftlength, seg_stride=sp.overlaplength, avg_method='median-mean')
+    psd_l_interp = pycbc.psd.interpolate(psd_l, delta_f=1.0 / sp.duration)
+
+    dataidx = 0
+    for tc in tclist_for_short_segment:
+        tini = tc - sp.duration / 2
+        tfin = tc + sp.duration / 2
+        if (timestamps.start_time < tini) and (tfin < timestamps.end_time):
+            # Slice the data and window
+            strain_h = xh1.time_slice(tini, tfin) * window
+            strain_l = xl1.time_slice(tini, tfin) * window
+
+            # Calculate SNR
+            for i in range(sp.height_input):
+                rho_h = matched_filter(template_bank[i], strain_h, psd=psd_h_interp, low_frequency_cutoff=sp.low_frequency_cutoff)
+                rho_l = matched_filter(template_bank[i], strain_l, psd=psd_l_interp, low_frequency_cutoff=sp.low_frequency_cutoff)
+                snrlist[0, i] = torch.from_numpy(abs(rho_h).numpy())[sp.kcrop_left: sp.kcrop_right]
+                snrlist[1, i] = torch.from_numpy(abs(rho_l).numpy())[sp.kcrop_left: sp.kcrop_right]
+
+            dataavg = make_snrmap_coarse(snrlist, sp.kfilter).to(torch.float32)
+            torch.save(dataavg, f'{outdir}/inputs_{timestamps.start_time_str}_{int(sp.duration):d}_{dataidx:d}.pth')
+            dataidx += 1
+
+
+def main(args):
+
+    outdir = args.outdir
+    if_not_exist_makedir(outdir)
+    file_foreground = args.foreground
+    file_injection = args.injection
+
+    sp = SignalProcessingParameters(
+        duration=16,
+        tsegment=1,
+        fs=2048,
+        low_frequency_cutoff=20.0,
+        tukey_alpha=1.0 / 16.0,
+        tfft=4.0,
+        width_input=256,
+        height_input=256
+    )
+
+    # Get timestamp informations
+    segment_timestamp_list = get_timestamps(file_foreground, file_injection, sp)
+
+    # Make a template bank
+    approximant_tmp = 'IMRPhenomXPHM'
+    mcmin_tmp = 5.0
+    mcmax_tmp = 50.0
+    ngrid_mc = sp.height_input
+    mclist = np.logspace(np.log10(mcmin_tmp), np.log10(mcmax_tmp), ngrid_mc, endpoint=True)
+    eta = 0.25
+    a1 = 0.0
+    a2 = 0.0
+    template_bank = []
+    for i in range(ngrid_mc):
+        mass1 = mass1_from_mchirp_eta(mclist[i], eta)
+        mass2 = mass2_from_mchirp_eta(mclist[i], eta)
+        params_tmp = {
+            'approximant': approximant_tmp,
+            'mass1': mass1,
+            'mass2': mass2,
+            'spin1z': a1,
+            'spin2z': a2,
+            'f_lower': sp.low_frequency_cutoff,
+            'delta_f': 1.0 / sp.duration,
+            'f_final': sp.high_frequency_cutoff
+        }
+
+        hp_fd, _ = get_fd_waveform(**params_tmp)
+        template_bank.append(hp_fd)
+
+    # Run the main code
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [executor.submit(matchedfilter_core, file_foreground, file_injection, outdir, sp, ts) for ts in segment_timestamp_list]
+        results = [f.result() for f in futures]
+    print(results)
+
+
+# def main(args):
+
+#     outdir = args.outdir
+#     if_not_exist_makedir(outdir)
+#     file_foreground = args.foreground
+#     file_injection = args.injection
+
+#     # Parameters for template matching
+#     duration = 16
+#     tsegment = 1
+#     fs = 2048
+#     fhigh = fs / 2
+#     low_frequency_cutoff = 20.0
+#     fftlength = int(4 * fs)
+#     overlap_length = int(fftlength / 2)
+#     window = tukey(int(duration * fs), alpha=1.0 / 16.0)
+
+#     approximant_tmp = 'IMRPhenomXPHM'
+#     mcmin_tmp = 5.0
+#     mcmax_tmp = 50.0
+#     ngrid_mc = 256
+#     mclist = np.logspace(np.log10(mcmin_tmp), np.log10(mcmax_tmp), ngrid_mc, endpoint=True)
+#     eta = 0.25
+#     a1 = 0.0
+#     a2 = 0.0
+
+#     # Image parameters
+#     # height = ngrid_mc
+#     width_input = 256
+#     kfilter = int(fs * tsegment / width_input)
+#     kcrop_left = int(fs * (duration / 2 - 3 * tsegment / 4))
+#     kcrop_right = int(fs * (duration / 2 + 3 * tsegment / 4))
+#     width_before_smearing = kcrop_right - kcrop_left
+
+#     # Make a template bank
+#     template_bank = []
+#     for i in range(ngrid_mc):
+#         mass1 = mass1_from_mchirp_eta(mclist[i], eta)
+#         mass2 = mass2_from_mchirp_eta(mclist[i], eta)
+#         params_tmp = {
+#             'approximant': approximant_tmp,
+#             'mass1': mass1,
+#             'mass2': mass2,
+#             'spin1z': a1,
+#             'spin2z': a2,
+#             'f_lower': low_frequency_cutoff,
+#             'delta_f': 1.0 / duration,
+#             'f_final': fhigh
+#         }
+
+#         hp_fd, _ = get_fd_waveform(**params_tmp)
+#         template_bank.append(hp_fd)
+
+#     # Get start time and end time
+#     with h5py.File(file_foreground, 'r') as fo:
+#         x = fo['H1'].keys()
+#         start_time_strlist = [xsample for xsample in x]
+#         start_time_list = [int(s) for s in start_time_strlist]
+#         nsegment = len(start_time_list)
+#         end_time_list = [int(len(fo['H1'][start_time_strlist[i]]) / fs) + start_time_list[i] for i in range(nsegment)]
+
+#     # Get the injection time stamps
+#     with h5py.File(file_injection, 'r') as fo:
+#         tclist_from_hdf5 = fo['tc'][:]
+#     # Assign a segment to an injection
+#     tclist = [[] for _ in range(nsegment)]
+#     for idx in range(len(tclist_from_hdf5)):
+#         for n in range(nsegment):
+#             if (start_time_list[n] <= tclist_from_hdf5[idx]) * (tclist_from_hdf5[idx] <= end_time_list[n]):
+#                 tclist[n].append(tclist_from_hdf5[idx])
+
+#     # Run the main code
+#     snrlist = torch.zeros((2, ngrid_mc, width_before_smearing), requires_grad=False)
+#     dataidx = 0
+#     for n in range(nsegment):
+#         # Set time stampes
+#         start_time_str = start_time_strlist[n]
+#         start_time = start_time_list[n]
+#         end_time = end_time_list[n]
+#         tclist_for_short_segment = tclist[n]
+#         if args.offevent:
+#             tclist_for_short_segment = np.array(tclist_for_short_segment)
+#             tclist_for_short_segment = (tclist_for_short_segment[:-1] + tclist_for_short_segment[1:]) / 2
+
+#         # Load a hdf file.
+#         xh1 = load_timeseries(file_foreground, group=f'H1/{start_time_str}')
+#         xl1 = load_timeseries(file_foreground, group=f'L1/{start_time_str}')
+
+#         # Estimate PSD
+#         psd_h = pycbc.psd.welch(xh1, seg_len=fftlength, seg_stride=overlap_length, avg_method='median-mean')
+#         psd_h_interp = pycbc.psd.interpolate(psd_h, delta_f=1.0 / duration)
+#         psd_l = pycbc.psd.welch(xl1, seg_len=fftlength, seg_stride=overlap_length, avg_method='median-mean')
+#         psd_l_interp = pycbc.psd.interpolate(psd_l, delta_f=1.0 / duration)
+
+#         for tc in tclist_for_short_segment:
+#             tini = tc - duration / 2
+#             tfin = tc + duration / 2
+#             if (start_time < tini) and (tfin < end_time):
+#                 print(tc)
+#                 # Slice the data and window
+#                 strain_h = xh1.time_slice(tini, tfin) * window
+#                 strain_l = xl1.time_slice(tini, tfin) * window
+
+#                 # Calculate SNR
+#                 # rholist = []
+#                 for i in range(ngrid_mc):
+#                     rho_h = matched_filter(template_bank[i], strain_h, psd=psd_h_interp, low_frequency_cutoff=low_frequency_cutoff)
+#                     rho_l = matched_filter(template_bank[i], strain_l, psd=psd_l_interp, low_frequency_cutoff=low_frequency_cutoff)
+#                     # snrlist[i] = torch.from_numpy(((abs(rho_h)**2 + abs(rho_l)**2) ** 0.5).numpy().astype(np.float32))
+#                     snrlist[0, i] = torch.from_numpy(abs(rho_h).numpy())[kcrop_left: kcrop_right]
+#                     snrlist[1, i] = torch.from_numpy(abs(rho_l).numpy())[kcrop_left: kcrop_right]
+
+#                 dataavg = make_snrmap_coarse(snrlist, kfilter).to(torch.float32)
+#                 torch.save(dataavg, f'{outdir}/inputs_{int(duration):d}_{dataidx:d}.pth')
+#                 dataidx += 1
+#                 if dataidx==100:
+#                     import sys
+#                     sys.exit(1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate matched filter images by using MDC dataset')
