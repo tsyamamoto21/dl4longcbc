2626b76a8a4fa6b3e7c59a7936b974b668fa5f1c
tsyamamoto@pegasus <tak.guruguru.4@gmail.com>
2025-09-25 19:00:36 +0900

    fix

--- test.sh
+++ test.sh
@@ -2,18 +2,27 @@
 #------- qsub option -------
 #PBS -q gpu
 #PBS -A ML4GW
-#PBS -l elapstim_req=00:30:00
+#PBS -l elapstim_req=00:10:00
 #PBS -j o
 #PBS -o log/neuralnet/test.log
 
 #------- Program execution -------
 set -x
+MODELNAME=gmn0.5_ksize5-13_channels64_leakyrelu
 module load cuda/12.1.0
 cd $PBS_O_WORKDIR
 apptainer exec --nv --bind `pwd` dl4longcbc.sif ./test.py\
-    --outdir=data/model/model_250912/gmn10.0_ksize5-13_channels64_relu/test_cbc/\
-    --modeldir=data/model/model_250912/gmn10.0_ksize5-13_channels64_relu/\
+    --outdir=data/model/model_250912/$MODELNAME/test_cbc/\
+    --modeldir=data/model/model_250912/$MODELNAME/\
     --datadir=data/dataset_250911/test/\
     --ndata=10000\
     --batchsize=100\
     --cbc
+
+apptainer exec --nv --bind `pwd` dl4longcbc.sif ./test.py\
+    --outdir=data/model/model_250912/$MODELNAME/test_noise/\
+    --modeldir=data/model/model_250912/$MODELNAME/\
+    --datadir=data/dataset_250911/test/\
+    --ndata=10000\
+    --batchsize=100\
+    --noise
