82486d7b4a2d9599a2c5f7268abd1fe2ba002156
tsyamamoto@pegasus <tak.guruguru.4@gmail.com>
2025-11-25 22:54:11 +0900

    update

--- test.sh
+++ test.sh
@@ -2,26 +2,27 @@
 #------- qsub option -------
 #PBS -q gpu
 #PBS -A ML4GW
-#PBS -l elapstim_req=00:10:00
+#PBS -l elapstim_req=00:15:00
 #PBS -j o
 #PBS -o log/neuralnet/test.log
 
 #------- Program execution -------
 set -x
-MODELNAME=snrlowerbound15
+EXPERIMENTNAME=251112_retrain
+MODELNAME=smearing_ksize5-5_channels64_relu_3
 module load cuda/12.1.0
 cd $PBS_O_WORKDIR
 apptainer exec --nv --bind `pwd` dl4longcbc.sif ./test.py\
-    --outdir=data/model/model_250912/$MODELNAME/test_cbc/\
-    --modeldir=data/model/model_250912/$MODELNAME/\
+    --outdir=data/model/$EXPERIMENTNAME/$MODELNAME/test_cbc/\
+    --modeldir=data/model/$EXPERIMENTNAME/$MODELNAME/\
     --datadir=data/dataset_250911/test/\
     --ndata=10000\
     --batchsize=100\
     --cbc
 
 apptainer exec --nv --bind `pwd` dl4longcbc.sif ./test.py\
-    --outdir=data/model/model_250912/$MODELNAME/test_noise/\
-    --modeldir=data/model/model_250912/$MODELNAME/\
+    --outdir=data/model/$EXPERIMENTNAME/$MODELNAME/test_noise/\
+    --modeldir=data/model/$EXPERIMENTNAME/$MODELNAME/\
     --datadir=data/dataset_250911/test/\
     --ndata=10000\
     --batchsize=100\
